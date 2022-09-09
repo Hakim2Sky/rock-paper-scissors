d335de254f9e53d9b090afe95082348df2f89ce4
Hakim2Sky <m.hakim@outlook.com>
2022-09-09 20:20:30 +0200

    create playRound function and get choice from user

--- script.js
+++ script.js
@@ -13,4 +13,51 @@ function getComputerChoice() {
             return "Scissors";
     }
 }
-console.log(getComputerChoice())
+
+const playerSelection = prompt("choose between Rock, Paper and Scissors: ");
+const computerSelection = getComputerChoice();
+
+// function that compares between player and computer choices and returns the winner
+function playRound(playerSelection, computerSelection) {
+
+    let winMessage = `You win! ${playerSelection} beats ${computerSelection}`
+    let loseMessage = `You lose! ${computerSelection} beats ${playerSelection}`
+
+    if (playerSelection.toLowerCase() == "rock") {
+        switch (true) {
+            case (computerSelection == "Scissors"):
+                return winMessage;
+                break;
+            case (computerSelection == "Paper"):
+                return loseMessage;
+                break;
+            default :
+                return "its a tie"
+        }
+    } else if (playerSelection.toLowerCase() == "paper") {
+        switch (true) {
+            case (computerSelection == "Rock"):
+                return winMessage;
+                break;
+            case (computerSelection == "Scissors"):
+                return loseMessage;
+                break;
+            default :
+                return "its a tie"
+        }
+    } else if (playerSelection.toLowerCase() == "scissors"){
+        switch (true) {
+            case (computerSelection == "Paper"):
+                return winMessage;
+                break;
+            case (computerSelection == "Rock"):
+                return loseMessage;
+                break;
+            default :
+                return "its a tie"
+        }
+    } else {
+        return "something went wrong"
+    }
+}
+console.log(playRound(playerSelection, computerSelection))
